e325c662ced8c45aeafd6aed913e0824638c90bb
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-14 17:11:54 +0200

    Header and Footer components added along with styles in assets

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Nav, Navbar, Container, } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">
+                  <img src={logo} alt='ProShop'/>
+                  ProShop
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className="ms-auto">
+                      <Nav.Link href="/cart">
+                        <FaShoppingCart/>
+                      </Nav.Link>
+                      <Nav.Link href="/login">
+                        <FaUser/>Sign In
+                      </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
