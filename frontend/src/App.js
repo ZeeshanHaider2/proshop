b3312bebb685509a1868a45ad236982205fe0e33
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-15 13:12:20 +0200

    react-router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-
+//import HomeScreen from './screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
 const App = () => {
   return (
     <>
       <Header/>
       <main className="py-3">
         <Container>
-            <HomeScreen/>
+            <Outlet/>
         </Container>
       </main>
       <Footer/>
