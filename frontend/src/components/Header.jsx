c45c1cdf6e66f111b3d651e4a526bf01f05a546f
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-15 16:12:44 +0200

    react-router updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Nav, Navbar, Container, } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 import React from 'react'
 
@@ -8,19 +9,25 @@ const Header = () => {
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
+              <LinkContainer to="/">
+                <Navbar.Brand>
                   <img src={logo} alt='ProShop'/>
                   ProShop
                 </Navbar.Brand>
+              </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className="ms-auto">
-                      <Nav.Link href="/cart">
-                        <FaShoppingCart/>
+                    <LinkContainer to="/cart">
+                      <Nav.Link>
+                        <FaShoppingCart/>Cart
                       </Nav.Link>
-                      <Nav.Link href="/login">
-                        <FaUser/>Sign In
+                      </LinkContainer>
+                      <LinkContainer to="/login">
+                      <Nav.Link >
+                        <FaUser/> Sign In
                       </Nav.Link>
+                      </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,7 +16,7 @@ import HomeScreen from './screens/HomeScreen';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" element={<HomeScreen />}/>
+      <Route index={true} path="/" element={<HomeScreen />}/>
     </Route>
   )
 );
