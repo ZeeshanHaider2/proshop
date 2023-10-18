f77a93e1f3339331d2dfe8318bab36f9e7c726eb
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-19 00:37:35 +0200

    loader and message comp added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,8 @@ import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 //import axios from 'axios';
 import { useGetProductsQuery } from '../slices/productsApiSlice'
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
 
@@ -20,8 +22,8 @@ const HomeScreen = () => {
   return (
     <>
     { isLoading ? (
-      <h2>Loading........</h2>
-    ) : error ? (<div>{error?.data?.message || error.error}</div>) : (
+      <Loader/>
+    ) : error ? (<Message variant = 'danger'>{error?.data?.message || error.error}</Message>) : (
       <>
       <h1>Latest Products</h1>
       <Row>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -5,6 +5,8 @@ import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 //import axios from 'axios';
 import { useGetProductDetailsQuery} from '../slices/productsApiSlice'
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 
 const ProductScreen = () => {
@@ -27,9 +29,9 @@ const ProductScreen = () => {
      <Link className ="btn btn-light my-3" to='/'>Go Back</Link>
 
      {isLoading ? (
-      <h2>....Loading</h2>
+      <Loader/>
      ) : error ? (
-      <div>{error?.data?.message || error.error}</div>
+      <Message variant = 'danger'>{error?.data?.message || error.error}</Message>
      ) : (<Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid/>
