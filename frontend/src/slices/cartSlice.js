28a7c7cf261bad55b73bb1d2bf948278555e32c4
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-21 15:15:17 +0200

    cartSlice addToCart fixed

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -16,7 +16,7 @@ const cartSlice = createSlice({
        const existItem = state.cartItems.find((x)=> x._id === item._id);
 
        if(existItem){
-        state.cartItems = state.cartItems.map((x) => x._Id ===
+        state.cartItems = state.cartItems.map((x) => x._id ===
         existItem._id ? item : x);
        } else {
         state.cartItems = [...state.cartItems, item];
