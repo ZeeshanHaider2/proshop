d9b9da764fd59a611dd181f7ba4b5a71f17b51d6
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-22 11:31:54 +0200

    authMiddleware added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
@@ -8,11 +9,15 @@ import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 connectDB(); //connection to mongoDB
 
+
 const app = express();
 
-//Body parser middleware
+// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
