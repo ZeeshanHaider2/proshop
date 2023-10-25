a3070b54d29224c2ed17ebe7ab4b3b4a582c718b
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-25 21:46:43 +0200

    shipping component added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,8 +27,8 @@ const router = createBrowserRouter(
       <Route  path="/product/:id" element={<ProductScreen />}/>
       <Route  path="/cart" element={<CartScreen />}/>
       <Route  path="/login" element={<LoginScreen />}/>
-      <Route  path="/login" element={<LoginScreen />}/>
       <Route  path="/register" element={<RegisterScreen />}/>
+      <Route  path="/shipping" element={<ShippingScreen />}/>
     </Route>
   )
 );
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
 ? JSON.parse(localStorage.getItem('cart'))
-: {cartItems : []};
+: {cartItems : [], shippingAddress:{}, paymentMethod:'PayPal'};
 
 
 const cartSlice = createSlice({
@@ -27,10 +27,13 @@ const cartSlice = createSlice({
     removeFromCart:(state, action) => {
      state.cartItems = state.cartItems.filter((x)=> x._id !== action.payload)
      return updateCart(state);
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
     }
-    
    },
 });
 
 export default cartSlice.reducer;
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
