d8d54e26fa0541533131e226f6594b69e3790231
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-25 17:59:56 +0200

    userRegistration functionality added

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -4,18 +4,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 import Loader from'../components/Loader';
-import { useLoginMutation } from '../slices/usersApiSlice';
+import { useRegisterMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
 import { toast} from 'react-toastify';
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const [ login, { isLoading } ] = useLoginMutation();
+    const [ register, { isLoading } ] = useRegisterMutation();
     
     const { userInfo } = useSelector((state) => state.auth);
     
@@ -29,23 +32,38 @@ const LoginScreen = () => {
     }
     },[ userInfo, redirect, navigate]);
 
-    const submitHandler = async (e) => {
+    const submitHandler = async(e) => {
         e.preventDefault();
-        try{
-          const res = await login({ email,password }).unwrap();
-          dispatch(setCredentials({...res, }));
-          navigate(redirect);
-        } catch (err) {
-         toast.error(err?.data?.message || err.error);
+        if (password !== confirmPassword){
+            toast.error('Password do not match');
+            return;
+        }else{
+            try{
+                const res = await register({name,email,password}).unwrap();
+                dispatch(setCredentials({...res, }));
+                navigate(redirect);
+              } catch (err) {
+               toast.error(err?.data?.message || err.error);
+              }
         }
     };
 
   return (
     <FormContainer>
-    <h1>Sign In</h1>
+    <h1>Sign Up</h1>
 
     <Form onSubmit={submitHandler}>
-      <Form.Group className='my-3' controlId='email'>
+    <Form.Group  controlId='name' className='my-3'>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          type='text'
+          placeholder='Enter name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <Form.Group  controlId='email' className='my-3'>
         <Form.Label>Email Address</Form.Label>
         <Form.Control
           type='email'
@@ -55,7 +73,7 @@ const LoginScreen = () => {
         ></Form.Control>
       </Form.Group>
 
-      <Form.Group className='my-2' controlId='password'>
+      <Form.Group className='my-3' controlId='password'>
         <Form.Label>Password</Form.Label>
         <Form.Control
           type='password'
@@ -65,9 +83,19 @@ const LoginScreen = () => {
         ></Form.Control>
       </Form.Group>
 
+      <Form.Group className='my-3' controlId='confirmPassword'>
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control
+          type='password'
+          placeholder='Confirm password'
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
       <Button  type='submit' variant='primary' className="mt-2"
       disabled ={ isLoading }>
-          Sign In
+          Register
         </Button>
 
         { isLoading && <Loader/> }
@@ -75,9 +103,9 @@ const LoginScreen = () => {
 
      <Row className='py-3'>
         <Col>
-          New Customer?
-          <Link to= {redirect ? `/register?redirect=${redirect}`: '/register'}>
-            Register
+          Already have an account?{''}
+          <Link to= {redirect ? `/auth?redirect=${redirect}`: '/auth'}>
+            Login
           </Link>
         </Col>
       </Row>
@@ -86,4 +114,4 @@ const LoginScreen = () => {
   )
 }
 
-export default LoginScreen;
+export default RegisterScreen;
