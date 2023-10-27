a74d75794f8ff74d70303b95c5b6d76cec4b9c34
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-28 00:12:34 +0200

    order page added

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -10,8 +10,13 @@ export const orderApiSlice = apiSlice.injectEndpoints({
         body: {...order}
       }),
     }),
+    getOrderDetails: builder.query({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const {
-  useCreateOrderMutation} = orderApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery } = orderApiSlice;
