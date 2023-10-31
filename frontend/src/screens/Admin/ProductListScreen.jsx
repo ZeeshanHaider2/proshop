1b4d04fbf2322f90ceb5c4db2073a20184ca8782
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-10-31 19:32:24 +0100

    creating products

--- frontend/src/screens/Admin/ProductListScreen.jsx
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -16,21 +16,36 @@ import { toast } from 'react-toastify';
 const ProductListScreen = () => {
     const { data:products, isLoading, error, refetch } = useGetProductsQuery();
     
+    const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
+
     const deleteHandler = (id) => {
         console.log('delete', id);
     };
+
+    const createProductHandler =  async () => {
+      if (window.confirm('Are you sure you want to create a new product?'))
+      {
+        try{
+         await createProduct();
+         refetch();
+        }catch (err){
+          toast.error(err.data?.message || err.error);
+        }
+      }
+    }
   return <>
      <Row className='align-items-center'>
         <Col>
           <h1>Products</h1>
         </Col>
         <Col className='text-end'>
-          <Button className='btn-sm m-3'>
+          <Button className='btn-sm m-3'
+          onClick = {createProductHandler}>
             <FaEdit /> Create Product
           </Button>
         </Col>
      </Row>
-
+     {loadingCreate && <Loader/>}
      {isLoading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
         <>
           <Table striped hover responsive className='table-sm'>
