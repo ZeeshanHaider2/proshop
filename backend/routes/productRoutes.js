e0f7f6ded455c8d8dbdb899253ae30b7780fdd79
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-10 18:23:11 +0100

    reviews backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,7 +5,8 @@ import {
     getProductById,
     createProduct, 
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    createProductReview
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -18,5 +19,6 @@ router
 .get(getProductById)
 .put(protect, admin, updateProduct)
 .delete(protect,admin,deleteProduct);
+router.route('/:id/reviews').post(protect,createProductReview);
 
 export default router;
