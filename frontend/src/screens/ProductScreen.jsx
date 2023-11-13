436d10262092d5f7444a571e66be9dc5b8b4d20b
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-13 22:03:38 +0100

    Customer review FE fixed

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,93 +1,109 @@
 import { useState } from 'react';
-import {  useParams, useNavigate } from "react-router-dom";
-//import { useEffect, useState } from 'react';
-import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
-import Rating from '../components/Rating';
-//import axios from 'axios';
-import { useGetProductDetailsQuery, useCreateProductMutation} from '../slices/productsApiSlice'
-import Loader from "../components/Loader";
-import Message from "../components/Message";
-import {addToCart} from '../slices/cartSlice';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
 import { toast } from 'react-toastify';
+import {
+  useGetProductDetailsQuery,
+  useCreateReviewMutation,
+} from '../slices/productsApiSlice';
+import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+//import Meta from '../components/Meta';
+import { addToCart } from '../slices/cartSlice';
+
 const ProductScreen = () => {
-  
+  const { id: productId } = useParams();
 
-    const {id: productId} = useParams();
-    
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [qty, setQty] = useState(1);
+  const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
-    const [ rating, setRating ] = useState(0);
-    const [ comment, setComment ] = useState('');
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate('/cart');
+  };
 
-    const { data:product, isLoading, error, refetch } = useGetProductDetailsQuery(productId);
-    
-    const [ createReview, { isLoading: loadingProductReview}] = useCreateProductMutation();
-    
-    const { userInfo } = useSelector ((state) => state.auth);
+  const {
+    data: product,
+    isLoading,
+    refetch,
+    error,
+  } = useGetProductDetailsQuery(productId);
 
-    const addToCartHandler = () => {
-        dispatch(addToCart({...product, qty}));
-        navigate('/cart');
-      };
+  const { userInfo } = useSelector((state) => state.auth);
 
-      const submitHandler = async (e) => {
-        e.preventDefault();
-    
-        try {
-          await createReview({
-            productId,
-            rating,
-            comment,
-          }).unwrap();
-          refetch();
-          toast.success('Review created successfully');
-          setComment(0);
-          setComment('');
-        } catch (err) {
-          toast.error(err?.data?.message || err.error);
-        }
-      };
+  const [createReview, { isLoading: loadingProductReview }] =
+    useCreateReviewMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await createReview({
+        productId,
+        rating,
+        comment,
+      }).unwrap();
+      refetch();
+      toast.success('Review created successfully');
+      setRating(0);
+      setComment("");
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
 
   return (
     <>
-     <Link className ="btn btn-light my-3" to='/'>Go Back</Link>
-
-     {isLoading ? (
-      <Loader/>
-     ) : error ? (
-      <Message variant = 'danger'>{error?.data?.message || error.error}</Message>
-     ) : (
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
         <>
-     <Row>
-        <Col md={5}>
-            <Image src={product.image} alt={product.name} fluid/>
-        </Col>
-        <Col md={4}>
-            <ListGroup variant = 'flush'>
+          
+          <Row>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={3}>
+              <ListGroup variant='flush'>
                 <ListGroup.Item>
-                    <h3>{product.name}</h3>
+                  <h3>{product.name}</h3>
                 </ListGroup.Item>
-
                 <ListGroup.Item>
-                    <Rating value = {product.rating} text = {`${product.numReviews}reviews`}/>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
                 </ListGroup.Item>
-                    
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
                 <ListGroup.Item>
-                    Price: ${product.price}
+                  Description: {product.description}
                 </ListGroup.Item>
-
-                <ListGroup.Item>
-                    Description: {product.description}
-                </ListGroup.Item>
-            </ListGroup>
-        </Col>
-        <Col md={3}>
-        <Card>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
                 <ListGroup variant='flush'>
                   <ListGroup.Item>
                     <Row>
@@ -101,45 +117,50 @@ const ProductScreen = () => {
                     <Row>
                       <Col>Status:</Col>
                       <Col>
-                        <strong>{product.countInStock > 0 ? 'In Stock':'Out of Stock'} </strong>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
                       </Col>
                     </Row>
                   </ListGroup.Item>
 
+                  {/* Qty Select */}
                   {product.countInStock > 0 && (
                     <ListGroup.Item>
                       <Row>
                         <Col>Qty</Col>
                         <Col>
-                         <Form.Control
-                           as='select'
-                           value={qty}
-                           onChange={(e)=> setQty(Number(e.target.value))}>
-                            {[...Array(product.countInStock).keys()].map((x) => (
-                              <option key={ x + 1 } value= { x + 1 }>
-                                { x + 1 }
-                              </option>
-                            ))}
-                           </Form.Control>
+                          <Form.Control
+                            as='select'
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
                         </Col>
                       </Row>
                     </ListGroup.Item>
                   )}
 
                   <ListGroup.Item>
-                   <Button className='btn-block' type='button'
-                   disabled = { product.countInStock === 0}
-                   onClick = {addToCartHandler}
-                   >
-                     Add to Cart
-                   </Button>
+                    <Button
+                      className='btn-block'
+                      type='button'
+                      disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
+                    >
+                      Add To Cart
+                    </Button>
                   </ListGroup.Item>
                 </ListGroup>
-        </Card>
-        </Col>
-     </Row>
-     
-     <Row className='review'>
+              </Card>
+            </Col>
+          </Row>
+          <Row className='review'>
             <Col md={6}>
               <h2>Reviews</h2>
               {product.reviews.length === 0 && <Message>No Reviews</Message>}
@@ -203,9 +224,9 @@ const ProductScreen = () => {
             </Col>
           </Row>
         </>
-     )}
+      )}
     </>
   );
 };
 
-export default ProductScreen
+export default ProductScreen;
