0a7af2055301823c789c114a9cdd81a4be98504a
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-14 23:55:32 +0100

    pagination added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,8 +6,13 @@ import Product from '../models/productModel.js';
 // @access  Public
 
 const getProducts = asyncHandler(async(req, res) => {
-    const products = await Product.find({});
-    res.json(products);
+  const pageSize = 2;  
+  const page = Number(req.query.pageNumber) || 1;
+  const count = await Product.countDocuments();
+  const products = await Product.find({})
+  .limit(pageSize)
+  .skip(pageSize * (page - 1));
+    res.json({products, page, pages: Math.ceil(count/pageSize)});
 });
 // @desc    Fetch all product
 // @route   GET /api/product/:id
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 //import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import Product from '../components/Product';
 //import axios from 'axios';
 import { useGetProductsQuery } from '../slices/productsApiSlice'
@@ -8,16 +9,8 @@ import Message from '../components/Message';
 
 const HomeScreen = () => {
 
-  //const [ products, setProducts ] = useState([]);
-  // useEffect(() => {
-  //const fetchProducts = async() => {
-  //const { data } =  await axios.get('api/products');
-  //setProducts(data);
-  //};
-  //fetchProducts();
-   //},[]);
-
-  const { data:products, isLoading, error } = useGetProductsQuery();
+  const {pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -27,7 +20,7 @@ const HomeScreen = () => {
       <>
       <h1>Latest Products</h1>
       <Row>
-         {products.map((product) => (
+         {data.products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
            <Product product={product}/>
           </Col>
