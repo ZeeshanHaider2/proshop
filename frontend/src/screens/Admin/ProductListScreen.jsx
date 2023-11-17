2344390158d83912534ec30837b3540238517cfa
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-17 10:18:41 +0100

    pagination products completed

--- frontend/src/screens/Admin/ProductListScreen.jsx
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
-//import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
-//import Paginate from '../../components/Paginate';
+import Paginate from '../../components/Paginate';
 import {
   useGetProductsQuery,
   useDeleteProductMutation,
@@ -14,7 +14,8 @@ import {
 import { toast } from 'react-toastify';
 
 const ProductListScreen = () => {
-    const { data:products, isLoading, error, refetch } = useGetProductsQuery();
+    const { pageNumber } = useParams();
+    const { data, isLoading, error, refetch } = useGetProductsQuery({pageNumber});
     
     const [ createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
     
@@ -72,7 +73,7 @@ const ProductListScreen = () => {
                 </tr>
             </thead>
             <tbody>
-                {products.map((product)=>(
+                {data.products.map((product)=>(
                     <tr key={product._id}>
                         <td>{product._id}</td>
                         <td>{product.name}</td>
@@ -94,6 +95,11 @@ const ProductListScreen = () => {
                 ))}
             </tbody>
           </Table>
+          <Paginate 
+         pages={data.pages}
+         page={data.page}
+         isAdmin={true}
+         />
         </>
      )}
   </>
