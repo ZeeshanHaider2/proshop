eb4d06ebcb744dbfdfdafd52b9abfc6956504e32
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-18 17:07:36 +0100

    search box component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,12 @@
 //import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import Product from '../components/Product';
-//import axios from 'axios';
+import Product from '../components/Product'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import  Paginate from '../components/Paginate';
-
+import { Link } from 'react-router-dom';
 const HomeScreen = () => {
 
   const {pageNumber, keyword } = useParams();
@@ -15,6 +14,7 @@ const HomeScreen = () => {
 
   return (
     <>
+    { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
     { isLoading ? (
       <Loader/>
     ) : error ? (<Message variant = 'danger'>{error?.data?.message || error.error}</Message>) : (
