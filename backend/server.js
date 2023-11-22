9a3007a1b29ac5b3cbfbb7629abb46f98319abbf
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2023-11-22 23:39:46 +0100

    settings for production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
- res.send('testing API...');
-});
+//app.get('/', (req, res) => {
+ //res.send('testing API...');
+//});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,6 +35,26 @@ app.get('/api/config/paypal',(req,res) => res.send ({ clientId:process.env.PAYPA
 
 const __dirname = path.resolve(); //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    //const __dirname = path.resolve();
+   // app.use('/uploads', express.static('/var/data/uploads'));
+    //set static folder
+   app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+   // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    //const __dirname = path.resolve();
+   // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+
+
 app.use(notFound);
 app.use(errorHandler);
 
